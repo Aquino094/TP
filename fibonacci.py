4960e0393d0ef140cd7f13af7eeed7ffb8dee41f
Julian Aquino <julian.aquino086@gmail.com>
2016-06-08 10:39:15 -0300

    diseño fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+#lo primero es crear una lista con un valor adentro de 0
+#crear dos variables que sera x ,y que tendran valor de 0 y 1
+#otra variable con la catidad de sumas a realizar
+#hacer una funcion que recorra la lista con un rango determinado
+#agregar la suma del las variables adentro de la lista
+#crear otra variable que guarde la suma de x e y
+#cambiar los valores de x por y, y al y x la suma de las mismas
+#por ultimo mostrar la lista completa
